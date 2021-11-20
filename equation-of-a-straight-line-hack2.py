88aa607d2092776b58d18b792b894557a79abe07
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2021-11-20 23:29:56 +0530

    changing slope[m] related

---
+++ equation-of-a-straight-line-hack2.py
@@ -0,0 +1,61 @@
+import os
+import pygame
+import time
+from math import pi
+
+pygame.init()
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+
+
+size = [800, 600]
+screen = pygame.display.set_mode(size)
+
+#Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+
+
+blockSize = 20 #Set the size of the grid block
+for x in range(0, 800, blockSize):
+    for y in range(0, 600, blockSize):
+        rect = pygame.Rect(x, y, blockSize, blockSize)
+        pygame.draw.rect(screen, WHITE, rect, 1)
+
+
+
+
+while not done:
+ 
+    # This limits the while loop to a max of 10 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(10)
+     
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+
+
+
+
+    m = 1
+    y = 100
+    while True:
+        c = 10
+        y = y + 1
+        x = (y - c)/m
+        print (x,y)
+        if y < 600:
+            pygame.draw.line(screen, BLUE, [400, 200], [x,y], 1)
+        time.sleep(0.01)
+        pygame.display.flip()
+        m = m + 1
+        if m == 400:
+            break
+
+pygame.quit()
+
