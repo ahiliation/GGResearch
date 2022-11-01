ba199826477db855eacdf15fd65485d59e56dc4c
Jeffrin Thalakkottoor <jeffrin@rajagiritech.edu.in>
2022-11-01 22:03:32 +0530

    parallelogram and square related

--- test-for-a-quadrilateral-related.py
+++ test-for-a-quadrilateral-related.py
@@ -10,10 +10,10 @@ from colorama import Fore, Back, Style
 
 #stackoveflow
 
-p1 = numpy.array((3,4))
-p2 = numpy.array((6,7))
-p3 = numpy.array((9,4))
-p4 = numpy.array((6,1))
+p1 = numpy.array((4,5))
+p2 = numpy.array((7,6))
+p3 = numpy.array((4,3))
+p4 = numpy.array((1,2))
 p1p2 = numpy.linalg.norm(p1-p2)
 p2p3 = numpy.linalg.norm(p2-p3)
 p3p4 = numpy.linalg.norm(p3-p4)
@@ -22,14 +22,12 @@ p1p3 = numpy.linalg.norm(p1-p3)
 p2p4 = numpy.linalg.norm(p2-p4)
 #print("p1p2 =",p1p2)
 #print("p2p3 =", p2p3)
-#print("p1p3 =", p3p1)
+#print("p1p3 =", p1p3)
 
-if ((p1p2 ==  p2p3) and (p3p4 == p4p1) and (p1p3 == p2p4)):
-    print(Back.WHITE, Fore.BLACK + 'the given shape is a square')
+if ((p1p2 ==  p3p4) and (p2p3 == p4p1) and (p1p3 != p2p4) and (p1p2 != p2p3) and (p3p4 != p4p1)) :
+    print(Fore.BLACK, Back.WHITE + 'the given shape is a parallelogram')
 #print(Style.DIM + 'and in dim text')
     print(Style.RESET_ALL)
-else:
-    print("Not a square shape \n")
 
 
 
