1917fe2d5cf3f292a843b398bbf7e22f99e9e33c
Jeffrin Thalakkottoor <jeffrin@rajagiritech.edu.in>
2022-11-13 19:19:28 +0530

    finding an equation between x and v

---
+++ finding-an-equation-between-x-and-y.py
@@ -0,0 +1,25 @@
+import os
+import time
+import math 
+import numpy
+from sympy.solvers import solve
+from sympy import Symbol
+from sympy import exp, Eq
+from colorama import Fore, Back, Style 
+
+#x = symbols('x')
+x= Symbol('x')
+y= Symbol('y')
+#p1, p2, p3 = Point(0, 0), Point(100, 100), Point(100, 100)
+
+
+expr1 = exp((x - 3)**2 + (y - 6)**2)
+expr2 = exp((x + 3)**2 + (y - 4)**2)
+
+eq = Eq(expr2 - expr1,0)
+#sol = solve(eq)
+print(eq)
+
+
+
+
