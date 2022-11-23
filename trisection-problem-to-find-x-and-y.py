3768352362b960c92b65ad7bca81ee7b72b99819
Jeffrin Thalakkottoor <jeffrin@rajagiritech.edu.in>
2022-11-23 14:49:49 +0530

    miscalculation fixed

--- trisection-problem-to-find-x-and-y.py
+++ trisection-problem-to-find-x-and-y.py
@@ -2,7 +2,7 @@ import os
 import time
 import math 
 import numpy
-import sympy
+from sympy import Point, Line
 
 p = Point(4,-1)
 q = Point(-2,-3)
@@ -15,10 +15,14 @@ n2 = 1
 
 
 x1 = (m1*q[0] +  n1*p[0])/(m1+n1)
-y1 = (m2*q[1] + n2*q[1])/(m1+n1)  
+y1 = (m1*q[1] + n1*p[1])/(m1+n1)  
 
-print(" x1 y1 is ...",x1, y1)
+x2 = (m2*q[0] +  n2*p[0])/(m1+n1)
+y2 = (m2*q[1] + n2*p[1])/(m2+n2)
 
+
+print(" x1 and y1 is...",x1,"and", y1, "respectively")
+print(" x2 and y2 is...",x2,"and", y2, "respectively")
 
 
 
